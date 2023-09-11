42a5cca930527785458fcb2d0d7e742f96b49ad7
yinpeng <y960303802@163.com>
2023-09-11 11:10:22 +0800

    feat: 09-11 log

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
-package Go_Study
+package main
+
+import "fmt"
 
 /*
    -*- coding: utf-8 -*-
    @Author  : hakusai22
    @Time    : 2023/08/12 17:40
 */
+
+func main() {
+	fmt.Println("佛祖保佑,永无BUG")
+}
