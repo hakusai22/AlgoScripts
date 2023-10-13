259b70529fcacfd7d068ce2db6ac91d83b0bf3fb
yinpeng <y960303802@163.com>
2023-10-14 00:38:21 +0800

    feat: 10-14

---
+++ Algorithm/LC_daily/2023_10/136. 只出现一次的数字.py
@@ -0,0 +1,49 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')
+INFMAX = float('inf')
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def end(r=-1):
+    print(r)
+    exit()
+
+
+# --idea
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/10/14 00:35
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        map = Counter(nums)
+        for k, v in map.items():
+            if v == 1:
+                return k
+        return 0
