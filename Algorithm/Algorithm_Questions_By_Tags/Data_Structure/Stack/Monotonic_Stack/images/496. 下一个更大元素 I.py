76561aaf3e244c7348766e77e95d88b694d60fde
yinpeng <y960303802@163.com>
2023-10-18 14:26:47 +0800

    feat: 10-18 单调栈

---
+++ Algorithm/Algorithm_Questions_By_Tags/Data_Structure/Stack/Monotonic_Stack/images/496. 下一个更大元素 I.py
@@ -0,0 +1,81 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache, reduce
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')  # 负无穷
+INFMAX = float('inf')  # 正无穷
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+# 数值和字母进行转换 ord()函数是把字符转换成ASCII码 chr()函数是把ASCII码转换成字符
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+# lcm 最小公倍数 gcd 最大公约数
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+# 快速幂
+def qpow(x, y):
+    ans = 1
+    while y:
+        if y & 1:
+            ans *= x
+        x *= x
+        y >>= 1
+    return ans
+
+# 求组合数
+def Comb(n, m, p):
+    a = (factorial(n)) % p
+    b = (qpow(factorial(m), (p - 2), p)) % p
+    c = (qpow(factorial(n - m), (p - 2), p)) % p
+    return a * b * c % p
+
+# lucas求组合数
+def Lucas(n, m, p):
+    if m == 0:
+        return 1
+    return Comb(n % p, m % p, p) * Lucas(n // p, m // p, p) % p
+
+def rep():
+    a = list(map(int, input().split()))
+    return a
+
+def sep():
+    a = input().rstrip('\n')
+    return a
+
+# --idea  为nums2维护一个字典，key为当前元素，value为该元素的下一个比其大的值
+#         设置一个递减栈，当遇到更大的元素时，把栈里比他小的元素都放到字典中
+#         查找时只需要在字典中找。时间复杂度O(n+m) 空间复杂度O(m)
+
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/10/18 14:18
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        d = dict()
+        for num in nums2:
+            while stack and stack[-1] < num:
+                s = stack.pop()
+                d[s] = num
+            stack.append(num)
+        return [d[num] if num in d else -1 for num in nums1]
