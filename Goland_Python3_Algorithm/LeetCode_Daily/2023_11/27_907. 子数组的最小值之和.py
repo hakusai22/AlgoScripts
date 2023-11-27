f247f431c1cafe1e1669bd39503878f9bc4a79d6
yinpeng <y960303802@163.com>
2023-11-27 08:22:06 +0800

    feat: 27

---
+++ Goland_Python3_Algorithm/LeetCode_Daily/2023_11/27_907. 子数组的最小值之和.py
@@ -0,0 +1,103 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache, reduce
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')  # 负无穷
+INFMAX = float('inf')  # 正无穷
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+'''
+gcd(), ord(), chr(), lower(), upper() 最大公约数/ASCII字符数值/数值ASCII字符/小写/大写
+startswith(s), endswith(s), find(), index(), count(s)  字符串是否以s开始的/字符串是否以s结尾的/查找返回的是索引/获取索引
+isalpha(), isdigit(), space(),join()  判断是否全为字符/判断是否全为数字/判断是否为空格/拼接
+eval() 字符串转换成列表、元组或者字典/
+uniform(x, y), pow(x, y)# 随机生成下一个实数，它在[x,y]范围内/ x**y 运算后的值。
+字典推倒式 {key: len(key) for key in list}
+列表推倒式 [i for i in range(100) if i % 3 == 0] 可切片,可索引,可重复
+集合推倒式 {i ** 2 for i in (1, 2, 3)}  不可索引,不可切片,不可重复元素
+'''
+
+# 数值和字母进行转换 ord()函数是把字符转换成ASCII码 chr()函数是把ASCII码转换成字符
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+# lcm 最小公倍数 gcd 最大公约数
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+# 快速幂
+def qpow(x, y):
+    ans = 1
+    while y:
+        if y & 1:
+            ans *= x
+        x *= x
+        y >>= 1
+    return ans
+
+# 求组合数
+def Comb(n, m, p):
+    a = (factorial(n)) % p
+    b = (qpow(factorial(m), (p - 2), p)) % p
+    c = (qpow(factorial(n - m), (p - 2), p)) % p
+    return a * b * c % p
+
+# lucas求组合数
+def Lucas(n, m, p):
+    if m == 0:
+        return 1
+    return Comb(n % p, m % p, p) * Lucas(n // p, m // p, p) % p
+
+def rep():
+    a = list(map(int, input().split()))
+    return a
+
+def sep():
+    a = input().rstrip('\n')
+    return a
+
+# --idea 
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/11/27 00:26
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        # 左边界 left[i] 为左侧严格小于 arr[i] 的最近元素位置（不存在时为 -1）
+        left, st = [-1] * n, []
+        for i, x in enumerate(arr):
+            while st and arr[st[-1]] >= x:
+                st.pop()
+            if st:
+                left[i] = st[-1]
+            st.append(i)
+
+        # 右边界 right[i] 为右侧小于等于 arr[i] 的最近元素位置（不存在时为 n）
+        right, st = [n] * n, []
+        for i in range(n - 1, -1, -1):
+            while st and arr[st[-1]] > arr[i]:
+                st.pop()
+            if st:
+                right[i] = st[-1]
+            st.append(i)
+
+        ans = 0
+        for i, (x, l, r) in enumerate(zip(arr, left, right)):
+            ans += x * (i - l) * (r - i)
+        return ans % MOD
