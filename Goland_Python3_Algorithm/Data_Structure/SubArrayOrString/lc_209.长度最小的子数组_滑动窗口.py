c840fba078b755cb67fec065fcbe72d53ffd876c
yinpeng <y960303802@163.com>
2023-12-01 14:58:52 +0800

    feat: 12/01 滑动窗口

--- Goland_Python3_Algorithm/Data_Structure/SubArrayOrString/lc_209.长度最小的子数组_滑动窗口.py
+++ Goland_Python3_Algorithm/Data_Structure/SubArrayOrString/lc_209.长度最小的子数组_滑动窗口.py
@@ -72,6 +72,10 @@ class ListNode:
 SlidingWindow_TwoPointer 时间复杂度：O(n)
 """
 
+# 给定一个含有 n 个正整数的数组和一个正整数 target 。
+# 找出该数组中满足其总和大于等于 target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
         if not nums:
