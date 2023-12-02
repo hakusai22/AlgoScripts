27416cc2e6dfd3aa16433d5d084d4a13f10a9925
yinpeng <y960303802@163.com>
2023-12-02 11:14:24 +0800

    feat: 12/02 链表

---
+++ Goland_Python3_Algorithm/Data_Structure/Linked_List/141. 环形链表_快慢指针.py
@@ -0,0 +1,97 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache, reduce
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')  # 负无穷
+INFMAX = float('inf')  # 正无穷
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+'''
+heaqp
+deque
+permutations(arr,r) 返回的是一个长度为 r 的所有可能排列，无重复元素
+combinations(arr,r) 返回的是一个长度为r的组合，它是有序的，无重复元素
+bisect_left()等同 函数返回排序数组中值等于k的最左索引，如果没有，就返回插入后其索引
+bisect()和bis_right函数返回排序数组中值等于k的最右索引+1，如果没有，就返回插入后其索引
+gcd(), ord(), chr(), lower(), upper() 最大公约数/ASCII字符数值/数值ASCII字符/小写/大写
+startswith(s), endswith(s), find(), index(), count(s)  字符串是否以s开始的/字符串是否以s结尾的/查找返回的是索引/获取索引
+isalpha(), isdigit(), space(),join()  判断是否全为字符/判断是否全为数字/判断是否为空格/拼接
+eval() 字符串转换成列表、元组或者字典/
+uniform(x, y), pow(x, y)# 随机生成下一个实数，它在[x,y]范围内/ x**y 运算后的值。
+字典推倒式 {key: len(key) for key in list}
+列表推倒式 [i for i in range(100) if i % 3 == 0] 可切片,可索引,可重复
+集合推倒式 {i ** 2 for i in (1, 2, 3)}  不可索引,不可切片,不可重复元素
+'''
+
+# 数值和字母进行转换 ord()函数是把字符转换成ASCII码 chr()函数是把ASCII码转换成字符
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+# lcm 最小公倍数 gcd 最大公约数
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+# 快速幂
+def qpow(x, y):
+    ans = 1
+    while y:
+        if y & 1:
+            ans *= x
+        x *= x
+        y >>= 1
+    return ans
+
+# 求组合数
+def Comb(n, m, p):
+    a = (factorial(n)) % p
+    b = (qpow(factorial(m), (p - 2), p)) % p
+    c = (qpow(factorial(n - m), (p - 2), p)) % p
+    return a * b * c % p
+
+# lucas求组合数
+def Lucas(n, m, p):
+    if m == 0:
+        return 1
+    return Comb(n % p, m % p, p) * Lucas(n // p, m // p, p) % p
+
+# --idea 
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/12/02 10:37
+#
+# 给你一个链表的头节点 head ，判断链表中是否有环。
+# 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+# 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast is slow:
+                return True
+        return False
