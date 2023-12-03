0319ba4605c6fed508fd16520940f6eaf62f8b6b
yinpeng <y960303802@163.com>
2023-12-03 12:30:13 +0800

    feat: 回溯 子集型/组合型/排列型 三种类型题型

---
+++ Goland_Python3_Algorithm/Data_Structure/Backtrack/216. 组合总和 III_dfs.py
@@ -0,0 +1,96 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache, reduce
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')  # 负无穷
+INFMAX = float('inf')  # 正无穷
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+'''
+heaqp
+deque
+permutations(arr,r) 返回的是一个长度为 r 的所有可能排列，无重复元素
+combinations(arr,r) 返回的是一个长度为r的组合，它是有序的，无重复元素
+bisect_left()等同 函数返回排序数组中值等于k的最左索引，如果没有，就返回插入后其索引
+bisect()和bis_right函数返回排序数组中值等于k的最右索引+1，如果没有，就返回插入后其索引
+gcd(), ord(), chr(), lower(), upper() 最大公约数/ASCII字符数值/数值ASCII字符/小写/大写
+startswith(s), endswith(s), find(), index(), count(s)  字符串是否以s开始的/字符串是否以s结尾的/查找返回的是索引/获取索引
+isalpha(), isdigit(), space(),join()  判断是否全为字符/判断是否全为数字/判断是否为空格/拼接
+eval() 字符串转换成列表、元组或者字典/
+uniform(x, y), pow(x, y)# 随机生成下一个实数，它在[x,y]范围内/ x**y 运算后的值。
+字典推倒式 {key: len(key) for key in list}
+列表推倒式 [i for i in range(100) if i % 3 == 0] 可切片,可索引,可重复
+集合推倒式 {i ** 2 for i in (1, 2, 3)}  不可索引,不可切片,不可重复元素
+'''
+
+# 数值和字母进行转换 ord()函数是把字符转换成ASCII码 chr()函数是把ASCII码转换成字符
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+# lcm 最小公倍数 gcd 最大公约数
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+# 快速幂
+def qpow(x, y):
+    ans = 1
+    while y:
+        if y & 1:
+            ans *= x
+        x *= x
+        y >>= 1
+    return ans
+
+# 求组合数
+def Comb(n, m, p):
+    a = (factorial(n)) % p
+    b = (qpow(factorial(m), (p - 2), p)) % p
+    c = (qpow(factorial(n - m), (p - 2), p)) % p
+    return a * b * c % p
+
+# lucas求组合数
+def Lucas(n, m, p):
+    if m == 0:
+        return 1
+    return Comb(n % p, m % p, p) * Lucas(n // p, m // p, p) % p
+
+# --idea 
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/12/03 10:22
+#
+# 找出所有相加之和为 n 的 k 个数的组合，且满足下列条件：
+# 只使用数字1到9
+# 每个数字 最多使用一次
+# 返回 所有可能的有效组合的列表 。该列表不能包含相同的组合两次，组合可以以任何顺序返回。
+#
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        rn = []
+        def dfs(x,t,he,ge):         # 当前数字x,当前列表，列表的和,列表个数
+            if x > 10 or ge > k:
+                return
+            if he == n and ge == k:
+                rn.append(t)
+            if he < n:
+                dfs(x+1,t,he,ge)    #不选这个数
+                dfs(x+1,t+[x],he+x,ge+1)    # 选这个数
+        dfs(1,[],0,0)
+        return rn
