76e4361e42680c6d7a1112e45c76a14d5302ba59
yinpeng <y960303802@163.com>
2023-12-23 23:59:47 +0800

    feat: 1223

---
+++ Goland_Python3_Algorithm/LeetCode/7DayChallengeHard/1223/4. 寻找两个正序数组的中位数.go
@@ -0,0 +1,8 @@
+package _223
+
+/*
+   --idea
+   -*- coding: utf-8 -*-
+   @Author  : hakusai22
+   @Time    : 2023/12/23 15:51
+*/
---
+++ Goland_Python3_Algorithm/LeetCode/7DayChallengeHard/1223/4. 寻找两个正序数组的中位数_2.py
@@ -0,0 +1,109 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache, reduce
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')  # 负无穷
+INFMAX = float('inf')  # 正无穷
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+'''
+heaqp
+deque
+permutations(arr,r) 返回的是一个长度为 r 的所有可能排列，无重复元素
+combinations(arr,r) 返回的是一个长度为r的组合，它是有序的，无重复元素
+bisect_left()等同 函数返回排序数组中值等于k的最左索引，如果没有，就返回插入后其索引
+bisect()和bis_right函数返回排序数组中值等于k的最右索引+1，如果没有，就返回插入后其索引
+gcd(), ord(), chr(), lower(), upper() 最大公约数/ASCII字符数值/数值ASCII字符/小写/大写
+startswith(s), endswith(s), find(), index(), count(s)  字符串是否以s开始的/字符串是否以s结尾的/查找返回的是索引/获取索引
+isalpha(), isdigit(), space(),join()  判断是否全为字符/判断是否全为数字/判断是否为空格/拼接
+eval() 字符串转换成列表、元组或者字典/
+uniform(x, y), pow(x, y)# 随机生成下一个实数，它在[x,y]范围内/ x**y 运算后的值。
+字典推倒式 {key: len(key) for key in list}
+列表推倒式 [i for i in range(100) if i % 3 == 0] 可切片,可索引,可重复
+集合推倒式 {i ** 2 for i in (1, 2, 3)}  不可索引,不可切片,不可重复元素
+'''
+
+# 数值和字母进行转换 ord()函数是把字符转换成ASCII码 chr()函数是把ASCII码转换成字符
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+# lcm 最小公倍数 gcd 最大公约数
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+# 快速幂
+def qpow(x, y):
+    ans = 1
+    while y:
+        if y & 1:
+            ans *= x
+        x *= x
+        y >>= 1
+    return ans
+
+# 求组合数
+def Comb(n, m, p):
+    a = (factorial(n)) % p
+    b = (qpow(factorial(m), (p - 2), p)) % p
+    c = (qpow(factorial(n - m), (p - 2), p)) % p
+    return a * b * c % p
+
+# lucas求组合数
+def Lucas(n, m, p):
+    if m == 0:
+        return 1
+    return Comb(n % p, m % p, p) * Lucas(n // p, m // p, p) % p
+
+# --idea 
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/12/23 15:51
+
+"""
+给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+算法的时间复杂度应该为 O(log (m+n)) 。
+
+示例 1：
+输入：nums1 = [1,3], nums2 = [2]
+输出：2.00000
+解释：合并数组 = [1,2,3] ，中位数 2
+示例 2：
+
+输入：nums1 = [1,2], nums2 = [3,4]
+输出：2.50000
+解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+
+提示：
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-10^6 <= nums1[i], nums2[i] <= 10^6
+"""
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num = nums1 + nums2
+        num.sort()
+        n = len(num)
+        if n % 2 == 1:
+            return num[n // 2]
+        else:
+            return (num[n // 2] + num[n // 2 - 1]) / 2
