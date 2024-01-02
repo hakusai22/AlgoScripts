26497917d664acc188f34d6d186cc59a859c4755
yinpeng <y960303802@163.com>
2024-01-02 23:10:04 +0800

    feat: leetcode 01/02

---
+++ Goland_Python3_Algorithm/LeetCode_Daily/2024_01/02_466. 统计重复个数.go
@@ -0,0 +1,33 @@
+package _024_01
+
+/*
+   --idea
+   -*- coding: utf-8 -*-
+   @Author  : hakusai22
+   @Time    : 2024/01/02 23:09
+*/
+
+func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) (ans int) {
+	n := len(s2)
+	d := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		j := i
+		cnt := 0
+		for k := range s1 {
+			if s1[k] == s2[j] {
+				j++
+				if j == n {
+					cnt++
+					j = 0
+				}
+			}
+		}
+		d[i] = [2]int{cnt, j}
+	}
+	for j := 0; n1 > 0; n1-- {
+		ans += d[j][0]
+		j = d[j][1]
+	}
+	ans /= n2
+	return
+}
