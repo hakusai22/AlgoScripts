fab0c782f643ed96ac303179fbe34499d19c0f5e
yinpeng <y960303802@163.com>
2024-07-28 22:29:27 +0800

    feature: leetcode daily

---
+++ Algorithm/LeetCode/daily/2024_07/28_699_hard.py
@@ -0,0 +1,102 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby, pairwise
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache, cache, reduce
+from typing import List, Optional
+import sys
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')  # 负无穷
+INFMAX = float('inf')  # 正无穷
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+# @Author  : https://github.com/hakusai22
+# @Time    : 2024/07/28 22:00
+# @题目     :
+# @参考     :   https://leetcode.cn/problems/falling-squares/solutions/2859880/python3javacgotypescript-yi-ti-yi-jie-xi-ju2s/?envType=daily-question&envId=2024-07-28
+# 时间复杂度 :
+
+class Node:
+    def __init__(self, l, r):
+        self.left = None
+        self.right = None
+        self.l = l
+        self.r = r
+        self.mid = (l + r) >> 1
+        self.v = 0
+        self.add = 0
+
+class SegmentTree:
+    def __init__(self):
+        self.root = Node(1, int(1e9))
+
+    def modify(self, l, r, v, node=None):
+        if l > r:
+            return
+        if node is None:
+            node = self.root
+        if node.l >= l and node.r <= r:
+            node.v = v
+            node.add = v
+            return
+        self.pushdown(node)
+        if l <= node.mid:
+            self.modify(l, r, v, node.left)
+        if r > node.mid:
+            self.modify(l, r, v, node.right)
+        self.pushup(node)
+
+    def query(self, l, r, node=None):
+        if l > r:
+            return 0
+        if node is None:
+            node = self.root
+        if node.l >= l and node.r <= r:
+            return node.v
+        self.pushdown(node)
+        v = 0
+        if l <= node.mid:
+            v = max(v, self.query(l, r, node.left))
+        if r > node.mid:
+            v = max(v, self.query(l, r, node.right))
+        return v
+
+    def pushup(self, node):
+        node.v = max(node.left.v, node.right.v)
+
+    def pushdown(self, node):
+        if node.left is None:
+            node.left = Node(node.l, node.mid)
+        if node.right is None:
+            node.right = Node(node.mid + 1, node.r)
+        if node.add:
+            node.left.v = node.add
+            node.right.v = node.add
+            node.left.add = node.add
+            node.right.add = node.add
+            node.add = 0
+
+class Solution:
+    def fallingSquares(self, positions: List[List[int]]) -> List[int]:
+        ans = []
+        mx = 0
+        tree = SegmentTree()
+        for l, w in positions:
+            r = l + w - 1
+            h = tree.query(l, r) + w
+            mx = max(mx, h)
+            ans.append(mx)
+            tree.modify(l, r, h)
+        return ans
