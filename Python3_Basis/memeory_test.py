b1ba80852b8d2ad3d3a3677266145eb9589533f0
yinpeng <y960303802@163.com>
2024-08-04 19:21:00 +0800

    feature: cache  calculate

--- Python3_Basis/memeory_test.py
+++ Python3_Basis/memeory_test.py
@@ -1,4 +1,3 @@
-import time
 from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
 from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
 from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
@@ -9,8 +8,6 @@ from functools import lru_cache, cache, reduce
 from typing import List, Optional
 import sys
 
-import requests
-import sched
 from sortedcontainers import SortedList, SortedDict, SortedSet
 
 sys.setrecursionlimit(10001000)
@@ -26,20 +23,12 @@ ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 alps = 'abcdefghijklmnopqrstuvwxyz'
 
 # @Author  : https://github.com/hakusai22
-# @Time    : 2024/07/23 00:02
+# @Time    : 2024/08/01 14:45
 # @题目     :
 # @参考     :  
 # 时间复杂度 :
 
 if __name__ == '__main__':
-
-    print(0x56259e50c000 - 0x56259d78e000)
-    print(0x7fa1d064e000 - 0x7fa1d063d000)
-    print(0x7fa1d084d000 - 0x7fa1d064e000)
-    print(0x7fa1d084f000 - 0x7fa1d084d000)
-
-    print(0x56259df52000-0x56259d78e000)
-    print(0x00d94000-0x00d73000)
-    print(0x7efe3fffe000-0x7efe3c000000)
-
-
+    print(0x7f2a8fffd000-0x7f2a8c000000)
+    print(0x7f2a90000000-0x7f2a8fffd000)
+    print(0x7f2a93ffe000-0x7f2a90000000)
