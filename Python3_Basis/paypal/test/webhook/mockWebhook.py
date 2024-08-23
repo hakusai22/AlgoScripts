66a60ffab0d8ee9963039310b531e338fc041692
yinpeng <y960303802@163.com>
2024-08-23 16:27:42 +0800

    fix

--- Python3_Basis/paypal/test/webhook/mockWebhook.py
+++ Python3_Basis/paypal/test/webhook/mockWebhook.py
@@ -6,7 +6,7 @@ from paypal.authorization import get_test_http_headers
 
 if __name__ == '__main__':
 
-    data = '{ "url": "https://api-staging.bongmi.com/v1/subscription/paypal/webhook", "event_type": "BILLING.SUBSCRIPTION.CREATED", "resource_version": "1.0" }'
+    data = '{ "url": "www.xxx.com", "event_type": "BILLING.SUBSCRIPTION.CREATED", "resource_version": "1.0" }'
     response = requests.post('https://api-m.sandbox.paypal.com/v1/notifications/simulate-event', headers=get_test_http_headers(), data=data)
     print(response.status_code)
     print(response.text)
